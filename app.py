d571e3d450e42ee30f8e31dbe5f07a8377bd99c1
jongho baek <decrasse9001@gmail.com>
2023-11-29 01:13:57 +0900

    5 commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout='wide')
 # HTML 파일을 읽어오기
-with open("jeseol.html", "r",  encoding="utf-8") as f:
+with open("jeseolham.html", "r",  encoding="utf-8") as f:
     html_code = f.read()
 
 # Stremlit 앱에 HTML 표시
